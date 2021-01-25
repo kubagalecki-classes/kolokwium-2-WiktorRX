7b22d7af9059f0c34627fb7d95bd505e82ff223f
WiktorRX <73623657+WiktorRX@users.noreply.github.com>
2021-01-25 18:53:49 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -2,5 +2,45 @@
 
 #include "catch.hpp"
 #include "chrup.hpp"
+#include <iostream>
 
 // tutaj klasy Ogorek, Zielony i Kiszony
+class Ogorek
+{
+public:
+    virtual std::string chrup() =0;
+};
+
+class Zielony : public Ogorek
+{
+public:
+    std::string chrup() override 
+    { 
+        return Ogorek.chrupZielony();
+    }
+};
+
+class Kiszony : public Ogorek
+{
+public:
+    std::string chrup() override
+    {
+        return Ogorek.chrupKiszony(); 
+    }
+};
+
+std::string jedzOgorek(Ogorek*)
+{
+    std::string tekst; 
+    if (Ogorek* == Zielony) //
+    {
+        tekst = "Zielony: "; 
+        return  tekst + chrupZielony();
+    }
+
+    if (Ogorek* == Kiszony)
+    {
+        tekst = "Kiszony: ";
+        return tekst + chrupKiszony();
+    }
+}
