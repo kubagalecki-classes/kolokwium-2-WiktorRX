6668b67f28ecd5478824342e6bcc0d635da85423
WiktorRX <73623657+WiktorRX@users.noreply.github.com>
2021-01-25 17:44:34 +0100

    Update zad1.hpp

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -7,3 +7,28 @@
 #include <string>
 
 // tutaj klasa Warzywo
+class Warzywo
+{
+public:
+    Warzywo(const std::string& n, double c, int k)
+    {
+        nazwa = n;
+        cena  = c;
+        kolor = k;
+        liczba++;
+    }
+    ~Warzywo() { liczba--; }
+    static unsigned getVeg() { return liczba; }
+    void opis(std::ostream& s) const { s << nazwa << ": " << cena << ", " << kolor << "\n"; }
+    // w1.opis(s1);
+    // CHECK(s1.str() == "cukinia: 2.1, 42\n");
+
+private:
+    std::string nazwa;
+    double      cena;
+    int         kolor;
+    inline static unsigned liczba = 0; // const???
+
+
+
+};
